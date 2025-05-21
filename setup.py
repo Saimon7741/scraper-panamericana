492b47cd253e3add44ea74e300784cb05a121ce4
Saimon7741 <134959781+Saimon7741@users.noreply.github.com>
2025-05-21 23:41:21 +0000

    empaquetado de dependencias

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="edu_pad",
+    version="0.0.1",
+    author="Simon Lara",
+    author_email="simon.lara@est.iudigital.edu.co",
+    description="Este proyecto es para hacer scrapeo a la pagina web de panamericana",
+    install_requires=[
+        "pandas",
+        "request",
+        "streamlit",
+        "beautifulsoup4",
+        "openpyxl",
+        "lxml"
+    ]
+)
